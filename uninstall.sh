d67c7cba55b79c11ab681f9d53fd5ba455b54302
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2009-04-29 16:39:19 +0200

    Updated install scripts

--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,8 @@
 # This is a very simplistic uninstall scipt. Use with care!
 
 if [ -n $1 ] && [ "$1" = "--system" ]; then
-  rm -v /usr/bin/hi-*
-  rm -vr /usr/lib/python*/site-packages/hipart
+  rm -v /usr/local/bin/hi-*
+  rm -vr /usr/local/lib/python*/site-packages/hipart
 else
   if [ -z $PYTHONPATH ]; then
     echo 'WARNING: $PYTHONPATH is not defined, defaulting to \$HOME/lib/python'
