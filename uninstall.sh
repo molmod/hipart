58d2ddd01e92b57f9b1f6bb464b654b227eb416d
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2010-05-18 12:26:24 +0200

    Fixed error in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,10 +5,6 @@ if [ -n $1 ] && [ "$1" = "--system" ]; then
   rm -v /usr/local/bin/hi-*
   rm -vr /usr/local/lib/python*/site-packages/hipart
 else
-  if [ -z $PYTHONPATH ]; then
-    echo 'WARNING: $PYTHONPATH is not defined, defaulting to \$HOME/lib/python'
-    PYTHONPATH=$HOME/lib/python
-  fi
   rm -v $HOME/bin/hi-*
-  rm -vr $PYTHONPATH/hipart
+  rm -vr $HOME/lib/python/hipart
 fi
