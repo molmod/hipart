e19e46cd2f6f9e20a5dcb01dcf7f81d4f2adb13e
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2010-07-19 17:47:52 +0200

    Add forgotten file

---
+++ scripts/hi-spin-charges.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# HiPart is a software toolkit to analyse molecular densities with the hirshfeld partitioning scheme.
+# Copyright (C) 2007 - 2010 Toon Verstraelen <Toon.Verstraelen@UGent.be>
+#
+# This file is part of HiPart.
+#
+# HiPart is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# HiPart is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
+
+
+
+
+
+
+from hipart.opts import parse_command_line
+
+usage = """%prog computes atomic spin charges."""
+
+context, cache = parse_command_line(usage)
+cache.do_spin_charges()
+context.clean()
