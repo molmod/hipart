59648f8dc6e329969003e7642da4fdebd2862a0a
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2010-07-30 10:51:17 +0200

    install scripts are back

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This is a very simplistic uninstall scipt. Use with care!
+
+if [ -n $1 ] && [ "$1" = "--system" ]; then
+  rm -vr /usr/local/lib/python*/site-packages/hipart
+  rm -vr /usr/local/bin/hi-*.py
+else
+  rm -vr $HOME/lib/python/hipart
+  rm -vr $HOME/bin/hi-*.py
+fi
