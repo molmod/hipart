89fd7f6348cb843fbf9ec6d7ae1613c62b50be0e
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2010-11-16 13:55:35 +0100

    Improve uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,10 @@
 
 if [ -n $1 ] && [ "$1" = "--system" ]; then
   rm -vr /usr/local/lib/python*/site-packages/hipart
+  rm -vr /usr/local/lib/python*/site-packages/Hipart*
   rm -vr /usr/local/bin/hi-*.py
 else
   rm -vr $HOME/lib/python/hipart-*
+  rm -vr $HOME/lib/python/HiPart*
   rm -vr $HOME/bin/hi-*.py
 fi
